6508aedc0c8a96bc3b320af3c3a24a6865e5b0d8
Can Kalender <cankalender526@gmail.com>
2025-10-01 13:14:13 +0300

    Initial Commit

---
+++ ScreenCast/ScreenCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenCastApp.swift
+//  ScreenCast
+//
+//  Created by Can Kalender on 1.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
